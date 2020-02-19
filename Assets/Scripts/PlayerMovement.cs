cb590143b6e86147e7367260c15ece301903a01e
sebastian <sebastianwon114@gmail.com>
2020-02-18 19:30:28 -0500

    added wall running

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        println('hello world');
+        println('this is wall running');
     }
 
     // Update is called once per frame
