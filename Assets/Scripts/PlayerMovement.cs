b5e9550ccee8b58f13d5eaa1a0d7ec677aa5d6a9
sebastian <sebastianwon114@gmail.com>
2020-02-18 19:27:55 -0500

    added println

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        println('hello world');
     }
 
     // Update is called once per frame
